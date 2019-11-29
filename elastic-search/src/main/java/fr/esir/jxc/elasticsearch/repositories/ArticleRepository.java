a2b9de87a610d96ddb935831e2069d4ff34b4e4d
Kévin <kevin.philip@outlook.fr>
2019-11-29 15:56:55 +0100

    feature(ArticleRepo) : Adding article repository

---
+++ elastic-search/src/main/java/fr/esir/jxc/elasticsearch/repositories/ArticleRepository.java
@@ -0,0 +1,14 @@
+package fr.esir.jxc.elasticsearch.repositories;
+
+import fr.esir.jxc.domain.models.Article;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends ElasticsearchRepository<Article, String> {
+
+    Page<Article> findAll();
+
+}
