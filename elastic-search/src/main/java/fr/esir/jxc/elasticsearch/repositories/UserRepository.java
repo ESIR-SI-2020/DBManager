7cc8412e2d738fbad85ffa57d7cce932469e9169
Kévin <kevin.philip@outlook.fr>
2020-01-13 12:01:51 +0100

    update(UserRepo)

--- elastic-search/src/main/java/fr/esir/jxc/elasticsearch/repositories/UserRepository.java
+++ elastic-search/src/main/java/fr/esir/jxc/elasticsearch/repositories/UserRepository.java
@@ -3,12 +3,14 @@ package fr.esir.jxc.elasticsearch.repositories;
 import fr.esir.jxc.domain.models.User;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends ElasticsearchRepository<User, String> {
 
+    Page<User> findAll(Pageable page);
     Page<User> findAll();
 
 }
