223d3597f5f2bf029e98748aa3233863c9191640
Matthias <matthiaspinard@gmail.com>
2020-01-13 08:48:44 +0100

    modèle ArticleAdded pour les analytics

---
+++ domain/src/main/java/fr/esir/jxc/domain/models/analytics/ArticleAdded.java
@@ -0,0 +1,20 @@
+package fr.esir.jxc.domain.models.analytics;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@Document(indexName = "pocket", type="article_added")
+public class ArticleAdded {
+    @Id
+    private String id;
+    private Date creationDate;
+    private String url;
+    private String email;
+
+}
