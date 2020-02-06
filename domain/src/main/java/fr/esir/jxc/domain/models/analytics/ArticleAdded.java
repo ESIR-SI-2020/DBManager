b413b116b99a2a93d47527cb0a35ea9d27d96c51
oumar <balloumar49@hotmail.com>
2020-02-06 15:42:46 +0100

    fix issue on analytics types

--- domain/src/main/java/fr/esir/jxc/domain/models/analytics/ArticleAdded.java
+++ domain/src/main/java/fr/esir/jxc/domain/models/analytics/ArticleAdded.java
@@ -2,12 +2,14 @@ package fr.esir.jxc.domain.models.analytics;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Document(indexName = "pocket", type="article_added")
 public class ArticleAdded {
--- domain/src/main/java/fr/esir/jxc/domain/models/analytics/UserAdded.java
+++ domain/src/main/java/fr/esir/jxc/domain/models/analytics/UserAdded.java
@@ -4,12 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 @Data
